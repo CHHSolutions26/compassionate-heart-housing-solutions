e56d57b05d719cd681d5a0f2210658c4fb0820d0
CHHSolutions26 <info@compassionatehearthousingsolutions.com>
2026-07-10 23:39:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -283,7 +283,10 @@ app.get('/api/submissions.csv', (req, res) => {
   res.send(rows.join('\n'));
 });
 
-app.get('/admin', (req, res) => res.sendFile(path.join(__dirname, 'public', 'admin.html')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.get('/admin', (req, res) => { 
+  res.sendFile(path.join(__dirname, 'public', 'admin.html')));
+});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html')));
 });
 app.listen(PORT, () => console.log(`CHHS website running on port ${PORT}`));
